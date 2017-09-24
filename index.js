c1e223422f0126744b2763828780e228ce690681
Matt Daisley <mattdaisley@gmail.com>
2017-09-24 13:18:23 -0600

    remove unsubscribe after a feed event

--- index.js
+++ index.js
@@ -12,37 +12,35 @@ hub = "http://pubsubhubbub.appspot.com/";
 pubsub.listen(3000);
 
 pubsub.on("denied", function(data){
-console.log("Denied");
-console.log(data);
+  console.log("Denied");
+  console.log(data);
 });
 
 pubsub.on("subscribe", function(data){
-console.log("Subscribe");
-console.log(data);
+  console.log("Subscribe");
+  console.log(data);
 
-console.log("Subscribed "+topic+" to "+hub);
+  console.log("Subscribed "+topic+" to "+hub);
 });
 
 pubsub.on("unsubscribe", function(data){
-console.log("Unsubscribe");
-console.log(data);
+  console.log("Unsubscribe");
+  console.log(data);
 
-console.log("Unsubscribed "+topic+" from "+hub);
+  console.log("Unsubscribed "+topic+" from "+hub);
 });
 
 pubsub.on("error", function(error){
-console.log("Error");
-console.log(error);
+  console.log("Error");
+  console.log(error);
 });
 
 pubsub.on("feed", function(data){
-console.log(data)
-console.log(data.feed.toString());
-
-pubsub.unsubscribe(topic, hub);
+  console.log(data)
+  console.log(data.feed.toString());
 });
 
 pubsub.on("listen", function(){
-console.log("Server listening on port %s", pubsub.port);
-pubsub.subscribe(topic, hub);
+  console.log("Server listening on port %s", pubsub.port);
+  pubsub.subscribe(topic, hub);
 });
